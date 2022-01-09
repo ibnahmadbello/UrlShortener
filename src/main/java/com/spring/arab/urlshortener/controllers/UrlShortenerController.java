d50f0dcb17f0f5fe5dbc3b32d52887016f9f5d36
ibnahmadbello <justforlearningcode@gmail.com>
2022-01-09 23:49:21 +0100

    shortened url can be redirect now

--- src/main/java/com/spring/arab/urlshortener/controllers/UrlShortenerController.java
+++ src/main/java/com/spring/arab/urlshortener/controllers/UrlShortenerController.java
@@ -1,8 +1,16 @@
 package com.spring.arab.urlshortener.controllers;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +42,32 @@ public class UrlShortenerController {
 		urlErrorResponseDto.setStatus("404");
 		urlErrorResponseDto.setError("there was an error processing your request. please try again");
 		return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{shortLink}")
+	public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String shortLink, HttpServletResponse response) throws IOException{
+		if(StringUtils.isEmpty(shortLink)) {
+			UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+			urlErrorResponseDto.setError("Invalid Url");
+			urlErrorResponseDto.setStatus("400");
+			return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+		}
+		Url urlToReturn = urlService.getEncodedUrl(shortLink);
+		if(urlToReturn == null) {
+			UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+			urlErrorResponseDto.setError("Url does not exist or it might have expired!");
+			urlErrorResponseDto.setStatus("400");
+			return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+		}
+		if(urlToReturn.getExpirationDate().isBefore(LocalDateTime.now())) {
+			urlService.deleteShortLink(urlToReturn);
+			UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+			urlErrorResponseDto.setError("Url Expired. Please try generating a fresh one.");
+			urlErrorResponseDto.setStatus("200");
+			return new ResponseEntity<UrlErrorResponseDto>(urlErrorResponseDto, HttpStatus.OK);
+		}
+		response.sendRedirect(urlToReturn.getOriginalUrl());
+		return null;
 	}
 	
 }
